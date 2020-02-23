d9b970723a3eb236509fd6bc9f620639d20d9bdd
Jose Duarte <contato@cariocaweb.com.br>
2020-02-23 16:14:45 -0300

    Operações

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-// Criando Funções
 
+//Opertações matemáticas
+function somar(){
+    var campo1=parseInt(document.getElementById("campo1").value);
+    var campo2=parseInt(document.getElementById("campo2").value);
+
+/*    alert("campo1 = " + campo1);
+    alert("campo2 = " + campo2); */
+
+    var soma = campo1 + campo2;
+    var subtrair = campo1 - campo2;
+    var multiplicar = campo1 * campo2;
+    var dividir = campo1 / campo2;
+    alert("a soma de: " + campo1 + " + " + campo2 + " = " + soma);
+    alert("a subtração de: " + campo1 + " - " + campo2 + " = "  + subtrair);
+    alert("a multiplicação de: " + campo1 + " x " + campo2 + " = " + multiplicar);
+    alert("a divisão de: " + campo1 + " / " + campo2 + " = " + dividir);
+
+}
+
+
+/*
 function adicionarIngrediente(){
     var ing = document.getElementById("ingrediente").value;
     var listahtml = document.getElementById("lista").innerHTML;
@@ -22,3 +42,4 @@ function tocarNome(nome, idade){ // função simples
 
     area.innerHTML = nome + " " + texto + " tem " + idade + " anos";
 }
+*/
