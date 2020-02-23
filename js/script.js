fb18e45ff69024a723f61ce6da198b033f4bdb7e
Jose Duarte <contato@cariocaweb.com.br>
2020-02-23 15:54:19 -0300

    Modificando uma lista

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 // Criando Funções
 
+function adicionarIngrediente(){
+    var ing = document.getElementById("ingrediente").value;
+    var listahtml = document.getElementById("lista").innerHTML;
+
+    listahtml = listahtml + "<li>"+ing+"</li>";
+    document.getElementById("lista").innerHTML = listahtml;
+}
+
 function tocarDiv(){ // função simples
     var area = document.getElementById("area");
     var texto = prompt("Qual o seu nome?");
